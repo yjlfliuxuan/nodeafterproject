75ba2a6754ce3497ac1a7cfcb6c925511ddde513
yjlfliuxuan <2226802518@qq.com>
2018-11-24 12:12:27 +0800

    写了头主页头部样式左边栏menu菜单

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,13 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.get("/users.html",function(req,res){
+  res.render("users");
+})
+router.get("/brand.html",function(req,res){
+  res.render("brand");
+})
+router.get("/phone.html",function(req,res){
+  res.render("phone");
+})
 module.exports = router;
