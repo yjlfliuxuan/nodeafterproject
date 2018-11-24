fdd7777d6139119c9ec1f825dd2807048ee716b4
yjlfliuxuan <2226802518@qq.com>
2018-11-24 18:32:11 +0800

    已完成登录页的基本样式搭建

--- routes/index.js
+++ routes/index.js
@@ -14,4 +14,10 @@ router.get("/brand.html",function(req,res){
 router.get("/phone.html",function(req,res){
   res.render("phone");
 })
+router.get("/login.html",function(req,res){
+  res.render("login");
+})
+router.get("/register.html",function(req,res){
+  res.render("register");
+})
 module.exports = router;
